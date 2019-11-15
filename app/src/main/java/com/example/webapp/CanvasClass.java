59b01453c96bab11734716fdc706482778a765a5
Joshua Rosenthal <jrosen52@binghamton.edu>
2019-11-15 14:35:51 -0500

    add timer

--- app/src/main/java/com/example/webapp/CanvasClass.java
+++ app/src/main/java/com/example/webapp/CanvasClass.java
@@ -6,10 +6,17 @@ import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.Random;
+
 public class CanvasClass extends SurfaceView implements SurfaceHolder.Callback
 {
     private SurfaceHolder surfaceHolder = null;
     private Paint paint = null;
+    private float circleX = 0;
+    private float circleY = 0;
+    Random r = new Random();
+    public int xCoor = r.nextInt((1000 - 30) + 1) + 30;
+    public int yCoor = r.nextInt((1500 - 30) + 1) + 30;
 
     public CanvasClass(Context context) {
         super(context);
